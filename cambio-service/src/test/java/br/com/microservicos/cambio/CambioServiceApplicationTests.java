64a5989c12478ed3ba7c9a42c0e44a81d7ba9e5d
Daniel K I Ikeuchi <daniel.ikeuchi@gmail.com>
2022-12-17 09:42:17 -0300

    Criacao Cambio Service

---
+++ cambio-service/src/test/java/br/com/microservicos/cambio/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservicos.cambio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
